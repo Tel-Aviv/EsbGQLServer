2d7d369d583906c919a0bad18597f5994d8b9ed4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-20 12:16:54 +0200

    Merge branch 'master' of https://github.com/Tel-Aviv/EsbGQLServer

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,2 +1,35 @@
+import rp from 'request-promise';
+import { GraphQLError } from 'graphql/error';
+
+const services = [{
+    id: 1,
+    name: 'soccer',
+    address: 'ws://'
+  }, {
+    id: 2,
+    name: 'baseball',
+    address: 'gql://'
+}];
+
 export const resolvers = {
+
+  Query: {
+    services: (root, args, context) => {
+
+      // 'context' is optional parameter passed to graphqlHTTP middleware.
+      // According to express-graphql GitHub repository documentation (https://github.com/graphql/express-graphql#options)
+      // this parameter is arbitrary value passed to resolvers.
+      // The most important part of this invokation is following statement:
+      // "If <i>context<i> is nor provided, the <i>request</i> object is passed as the context.
+      //
+      // So because we din't touched 'context' object on Express, we get it here as the request
+      // parameter - named context
+      return services;
+    },
+    service: (root, {name}) => {
+        const serice = services.find(service => service.name == name);
+        return serice;
+    }
+  }
+
 }
--- schemas/schema.js
+++ schemas/schema.js
@@ -3,3 +3,26 @@ import {
 } from 'graphql-tools';
 
 import { resolvers } from './resolvers.js';
+
+const typeDefs = `
+
+type Service {
+    id: ID!
+    name: String
+    address: String!
+    description: String
+    sla: Int
+    when_published: String #Date
+    affiliation: [String]
+    available: Boolean
+}
+
+# This type specifies the entry points into our API.
+type Query {
+    services: [Service]
+    service(name: String): Service
+}
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+export { schema };
