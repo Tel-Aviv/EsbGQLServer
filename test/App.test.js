32d993a394e3db42aa240f7267f0b5b345da27ec
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-20 11:17:24 +0200

    Initial commit

---
+++ test/App.test.js
@@ -0,0 +1,11 @@
+test('Test App component', () => {
+
+  beforeAll(() => {
+    console.log('Before All');
+
+  });
+
+  // const component = renderer.create(<App />);
+  // let tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+});
