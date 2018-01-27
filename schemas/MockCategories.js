6aecdb3da98e8b5de3620d14d6897a4f1e557304
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-27 19:36:55 +0200

    Add files via upload

---
+++ schemas/MockCategories.js
@@ -0,0 +1,35 @@
+const mockCategories = [{
+  CategoryId: 1,
+  CategoryName: "משרד התחבורה",
+  Description: "Description 1"
+}, {
+  CategoryId: 2,
+  CategoryName: "שירותי מיקום",
+  Description: "Description 2"
+}, {
+  CategoryId: 3,
+  CategoryName: "דיגיתל",
+  Description: "Description 3"
+}, {
+  CategoryId: 4,
+  CategoryName: "מחו\"ג",
+  Description: "Description 4"
+}, {
+  CategoryId: 5,
+  CategoryName: "עירייה זמינה",
+  Description: "Description 5"
+}, {
+  CategoryId: 6,
+  CategoryName: "עמ\"ל",
+  Description: "Description 6"
+}, {
+  CategoryId: 10,
+  CategoryName: "טלאול",
+  Description: "Description 10"
+}, {
+  CategoryId: 12,
+  CategoryName: "תשתיות אינטגרציה",
+  Description: "Description 12"
+}];
+
+module.exports = mockCategories;
---
+++ schemas/MockServices.js
@@ -0,0 +1,15 @@
+const mockServices = [{
+    id: 111,
+    categoryId: 1,
+    name: 'Service Name A',
+    address: 'http://iis07/apps/s1.svc',
+    sla: 200
+  }, {
+    id: 112,
+    categoryId: 2,
+    name: 'Service Name B',
+    address: 'http://iis08/apps/s2.svc',
+    sla: 150
+}];
+
+module.exports = mockServices;
