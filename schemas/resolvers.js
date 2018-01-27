d6857bf9997f6eac2dcd81502d925a6c52fe3fa2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-01-27 19:41:27 +0200

    Better mockes

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,52 +1,158 @@
+import _ from 'lodash';
 import rp from 'request-promise';
 import { GraphQLError } from 'graphql/error';
+import mockServices from './MockServices';
+import mockCategories from './MockCategories';
 
-const categories = [{
-  id: 1,
-  name: "משרד התחבורה"
-}, {
-  id: 2,
-  name: "שירותי מיקום"
-}, {
-  id: 3,
-  name: "דיגיתל"
-}, {
-  id: 4,
-  name: "מחו\"ג"
-}, {
-  id: 5,
-  name: "עירייה זמינה"
-}, {
-  id: 6,
-  name: "עמ\"ל"
-}, {
-  id: 10,
-  name: "טלאול"
-}, {
-  id: 12,
-  name: "תשתיות אינטגרציה"
-}];
+const MOCK_TIMEOUT = 1000;
 
-const services = [{
-    id: 1,
-    categoryId: 1,
-    name: 'Service Name A',
-    address: 'http://iis07/apps/s1.svc',
-    sla: 200
-  }, {
-    id: 2,
-    categoryId: 2,
-    name: 'Service Name B',
-    address: 'http://iis08/apps/s2.svc',
-    sla: 150
-}];
+class EsbAPI {
+
+  static getCategory(categoryId: number) {
+
+    return new Promise( (resolve, reject) => {
+
+        setTimeout( () => {
+
+          let category = mockCategories.find(category => category.CategoryId == categoryId);
+          resolve(category);
+
+        }, MOCK_TIMEOUT);
+    })
+
+  }
+
+  static getAllCategories() {
+
+    return new Promise( (resolve, reject) => {
+
+      setTimeout( () => {
+
+        resolve(_.assign([], mockCategories));
+
+      }, MOCK_TIMEOUT);
+    })
+  }
+
+  static getAllServices() {
+
+    return new Promise( (resolve, reject) => {
+
+      setTimeout( () => {
+
+        resolve(_.assign([],services));
+
+      }, MOCK_TIMEOUT);
+    });
+ }
+
+  static getServicesByCategoryId(categoryId: number) {
+
+    return new Promise( (resolve, reject) => {
+
+        setTimeout( () => {
+
+           let categorizedServices = mockServices.filter( (service) => {
+             return service.categoryId == categoryId;
+           });
+
+           resolve(_.assign([], categorizedServices));
+
+        }, MOCK_TIMEOUT);
+    });
+  }
+};
+
+function isMockMode() {
+
+  let mockToken = process.argv.find( (arg) => {
+    return arg == "--mock"
+  })
+
+  return mockToken;
+}
 
 export const resolvers = {
 
   Query: {
 
     categories: (root, args, context) => {
-      return categories;
+
+      if( isMockMode() ) {
+
+        let promise = EsbAPI.getAllCategories();
+        return promise.then( res => {
+
+            return res.map( (category) => {
+
+              return {
+                id: category.CategoryId,
+                name: category.CategoryName,
+                description: category.Description
+              }
+            });
+
+        });
+
+      } else {
+
+        const url = 'http://esb01/ESBUddiApplication/api/Categories';
+
+        return rp({
+          uri: url,
+          headers: {
+            'User-Agent': 'GraphQL'
+          },
+          json: true
+        }).then( res => {
+
+          return res.map( (category) => {
+            return {
+              id: category.CategoryId,
+              name: category.CategoryName,
+              description: category.Description
+            }
+          });
+
+        }).catch( (data) => {
+          return Promise.reject(data.error.message);
+        })
+
+        return categories;
+      }
+    },
+
+    category: (root, {id}) => {
+
+      if( isMockMode() ) {
+
+        let promise = EsbAPI.getCategory(id);
+        return promise.then ( res => {
+
+          return {
+            id: res.CategoryId,
+            name: res.CategoryName,
+            description: res.Description
+          }
+
+        });
+      } else {
+        const url = 'http://esb01/ESBUddiApplication/api/Categories/' + id;
+
+        return rp({
+          uri: url,
+          headers: {
+            'User-Agent': 'GraphQL'
+          },
+          json: true
+        }).then( category => {
+          return {
+            id: category.CategoryId,
+            name: category.CategoryName,
+            description: category.Description
+          }
+        })
+      }
     },
 
     services: (root, args, context) => {
@@ -60,18 +166,52 @@ export const resolvers = {
       // So because we din't touched 'context' object on Express, we get it here as the request
       // parameter - named context
 
-      if( !args.categoryId )
-        return services;
-
       const categoryId = args.categoryId;
 
-      return services.filter( (service) => {
-        return service.categoryId == categoryId;
-      })
+      if( isMockMode() ) {
+
+        if( !args.categoryId ) {
+
+          let promise = EsbAPI.getAllServices();
+          return promise.then( _services => {
+            return _services;
+          });
+
+        } else {
+
+          let promise = EsbAPI.getServicesByCategoryId(categoryId);
+          return promise.then( _services => {
+            return _services;
+          });
+
+        }
+      } else {
+
+        const url = ( !args.categoryId ) ?
+                 'http://m2055895-w7/ESBUddiApplication/api/Services'
+                 : 'http://m2055895-w7/ESBUddiApplication/api/Services?categoryId=' + categoryId;
+
+        return rp({
+          uri: url,
+          headers: {
+            'User-Agent': 'GraphQL'
+          },
+          json: true
+        }).then( res => {
+
+          return res;
+
+        }).catch( (data) => {
+          return Promise.reject(data.error.message);
+        })
+
+      }
+
     },
+
     service: (root, {name}) => {
-        const serice = services.find(service => service.name == name);
-        return serice;
+        const service = services.find(service => service.name == name);
+        return service;
     }
   }
 
