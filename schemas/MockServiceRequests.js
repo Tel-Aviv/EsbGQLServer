2f30106fea7efab8e20630f409bbc227978f41cb
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-06 19:54:29 +0200

    Update MockServiceRequests.js

--- schemas/MockServiceRequests.js
+++ schemas/MockServiceRequests.js
@@ -2,15 +2,25 @@ import casual from 'casual';
 
 const mockServiceRequests = [{
   id: 400,
-  ServiceRequestId: casual.integer(200, 2000),
-  operationName: casual.title,
-  created: new Date(),
+  RequestId: casual.integer(200, 2000),
+  ServiceUri: casual.url,
+  ServiceName: casual.title,
+  OperationName: casual.title,
+  ServiceSdl: casual.url,
+  ServiceCategoryId: casual.integer(1, 50),
+  ServiceSoapAction: casual.url,
+  PublishRequestDate: new Date(),
   ServiceUri: casual.url
 }, {
   id: 401,
-  ServiceRequestId: casual.integer(200, 2000),
-  operationName: casual.title,
-  created: new Date(),
+  RequestId: casual.integer(200, 2000),
+  ServiceUri: casual.url,
+  ServiceName: casual.title,
+  OperationName: casual.title,
+  ServiceSdl: casual.url,
+  ServiceCategoryId: casual.integer(1, 50),
+  ServiceSoapAction: casual.url,
+  PublishRequestDate: new Date(),
   ServiceUri: casual.url
 }];
 
