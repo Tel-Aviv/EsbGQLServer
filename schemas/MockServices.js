9782369ae5c86339edce66abd32947714445e894
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-02-19 19:53:00 +0200

    Update MockServices.js

--- schemas/MockServices.js
+++ schemas/MockServices.js
@@ -1,26 +1,23 @@
 import casual from 'casual';
 
 const mockServices = [{
-    id: 111,
-    ServiceId: () => casual.integer(200, 2000),
+    Id: () => casual.integer(200, 2000),
     CategoryId: 1,
-    ServiceName: 'Service Name A',
-    ServiceUrl: casual.url,
-    ServiceSLA: 200
+    Name: 'Service Name A',
+    Url: casual.url,
+    ExpectedSla: 200
   }, {
-    id: 112,
-    ServiceId: () => casual.integer(200, 2000),
+    Id: () => casual.integer(200, 2000),
     CategoryId: 2,
-    ServiceName: 'Service Name B',
-    ServiceUrl: casual.url,
-    ServiceSLA: 150
+    Name: 'Service Name B',
+    Url: casual.url,
+    ExpectedSla: 150
 }, {
-  id: 113,
-  ServiceId: () => casual.integer(200, 2000),
+  Id: () => casual.integer(200, 2000),
   CategoryId: 2,
-  ServiceName: 'Service Name C',
-  ServiceUrl: casual.url,
-  ServiceSLA: 140
+  Name: 'Service Name C',
+  Url: casual.url,
+  ExpectedSla: 140
 }];
 
 module.exports = mockServices;
