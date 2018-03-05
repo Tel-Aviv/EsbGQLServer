2d58be34a7f79c47bef76108df9c7d650bddc3e4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-05 13:13:38 +0200

    Update create.js

--- elastic/create.js
+++ elastic/create.js
@@ -3,7 +3,19 @@ import client from './connection.js';
 
 client.indices.create({
   index: 'esb_ppr',
-  timeout: '10m'
+  timeout: '10m',
+  body: {
+    "settings": {
+      "analysis": {
+        "normalizer": {
+          "lowercase_normalizer":{
+            "type": "custom",
+            "filter": ["lowercase"]
+          }
+        }
+      }
+    },
+  }
 }).then( resp => {
   console.log("create: ",resp);
 }). catch( err => {
