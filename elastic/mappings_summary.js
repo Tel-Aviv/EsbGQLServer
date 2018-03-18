72c913bdf2e086b7c622de4b92d10da307d26915
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-18 15:45:59 +0200

    Add files via upload

---
+++ elastic/mappings_summary.js
@@ -0,0 +1,56 @@
+// @flow
+import client from './connection.js';
+
+client.indices.putMapping({
+  index: 'esb_ppr_summary',
+  type: 'summary',
+  timeout: '10m',
+  body: {
+    "properties": {
+      'trace_Date': {
+        'type': 'date',
+      },
+      'message_guid': {
+        'type': 'keyword'
+      },
+      'esb_Latency': {
+        'type': 'integer',
+      },
+      'transport_Latency': {
+        'type': 'integer',
+      },
+      'service_Latency': {
+          'type': 'integer'
+      },
+      'status': {
+        'type': 'keyword',
+        "normalizer": "lowercase_normalizer"
+      },
+      'service_id': {
+        'type': 'short'
+      },
+      'service_name': {
+        'type': 'text',
+        "analyzer": "partial_words_analyzer"
+      },
+      'environment': {
+        'type': 'short'
+      },
+      'client_ip': {
+        'type': 'ip'
+      },
+      'client_user': {
+        "type": "text",
+        "analyzer": "partial_words_analyzer"
+      }
+    }
+  }
+},
+function(err,resp,status){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(resp);
+    }
+});
