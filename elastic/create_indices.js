56bc1bcb145a25a7185d85e2bff5ae85308c7020
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-03-18 16:05:50 +0200

    Add files via upload

---
+++ elastic/create_indices.js
@@ -0,0 +1,46 @@
+// @flow
+import client from './connection.js';
+
+client.indices.create({
+  index: 'esb_ppr_raw',
+  timeout: '10m',
+  body: { }
+}).then( resp => {
+  console.log("create: ",resp);
+}). catch( err => {
+  console.log(err);
+});
+
+client.indices.create({
+  index: 'esb_ppr_summary',
+  timeout: '10m',
+  body: {
+    "settings": {
+      "analysis": {
+        "analyzer" : {
+          "partial_words_analyzer" : {
+            "tokenizer" : "partial_words_tokenizer"
+          }
+        },
+        "tokenizer" : {
+          "partial_words_tokenizer" : {
+            "type": "edge_ngram",
+            "min_gram": 2,
+            "max_gram": 10,
+            "token_chars": ["letter", "digit"]
+          }
+        },
+        "normalizer": {
+          "lowercase_normalizer":{
+            "type": "custom",
+            "filter": ["lowercase"]
+          }
+        }
+      }
+    },
+  }
+}).then( resp => {
+  console.log("create: ",resp);
+}). catch( err => {
+  console.log(err);
+});
